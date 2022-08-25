615310c79aca2df34ded10e8fe8a211870aa0193
miguel680 <moonlighth3r@gmail.com>
2022-08-25 17:13:12 -0500

    prueba 4

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -17,9 +17,9 @@ function mayoriaDeEdad(edad) {
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
   if (edad >= 18) {
-  return "allowed";
+  return "Allowed";
 } else {
-  return "not allowed"
+  return "Not allowed"
 }
 }
   
@@ -30,11 +30,11 @@ function conection(status) {
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
   if (status >= 1) {
-  return "online";
+  return "Online";
 } else if (status >= 2) {
-  return "away";
+  return "Away";
 } else {
-  return "offline";
+  return "Offline";
 }
 }
 
